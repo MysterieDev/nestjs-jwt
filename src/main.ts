c153b73489dddad3f4dfef9b3b87cd5f24b64811
Mysteriedev <josefsle97@gmail.com>
2020-10-29 16:46:34 +0100

    add security

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
+import * as csurf from 'csurf';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //middleware
+  // TODO: use CSURF against xss
+  app.use(helmet());
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 250, // limit each IP to 250 requests per windowMs
+    }),
+  );
+
   // This prefixes all requests with API
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
